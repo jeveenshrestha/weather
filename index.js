c103c7b2923a2cab6f142439d05b9d9731cc4a6f
Jeveen Shrestha <jeveen2012shrestha@gmail.com>
2024-12-10 10:15:14 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import axios from "axios";
+import expressEjsLayouts from "express-ejs-layouts";
+
+const port = 3000;
+const API_KEY = "e4a771c10d7625cfcb64a8827c04eca4"
+
+const url = "https://api.openweathermap.org/data/2.5/weather";
+
+
+const app = express();
+app.use(expressEjsLayouts);
+
+app.set("view engine", 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+const config = {
+  params: {
+    q: "Oulu",
+    appid: API_KEY,
+    units: "metric",
+  }
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(url, config);
+    res.render("pages/index.ejs", {
+      weatherData: response.data
+    })
+  } catch (error) {
+    console.log(error.response.data)
+  }
+})
+
+app.post("/city", async (req, res) => {
+  const city = req.body.city;
+  const modifiedConfig = {
+    params: {
+      ...config.params,
+      q: city
+    }
+  }
+  try {
+    const response = await axios.get(url, modifiedConfig);
+    res.render("pages/index.ejs", {
+      weatherData: response.data,
+    })
+  } catch (error) {
+    console.log(error.response.data.message)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`)
+})
